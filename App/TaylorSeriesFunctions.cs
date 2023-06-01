6fb7eb15529c3b2b9e02c5afa67ba93fdfa81fac
Artyom Abramyan <artemabramyan7@icloud.com>
2023-06-01 14:14:45 +0300

    Переписал функции
    Теперь не возникает проблемы с числом членов суммы больше 17

--- App/TaylorSeriesFunctions.cs
+++ App/TaylorSeriesFunctions.cs
@@ -4,46 +4,64 @@ namespace App;
 
 public static class TaylorSeriesFunctions
 {
-	private static double SinTerm(double x, uint i) =>
-		(i % 2 == 0 ? -1 : 1) * Pow(x, 2 * i - 1) / Factorial(2 * i - 1);
-
-	public static double Sin(double x, uint n) =>
-		TaylorSeries(SinTerm, x, n);
-
-	private static double CosTerm(double x, uint i) =>
-		(i % 2 == 0 ? -1 : 1) * Pow(x, 2 * i - 2) / Factorial(2 * i - 2);
-
-	public static double Cos(double x, uint n) =>
-		TaylorSeries(CosTerm, x, n);
-
-	private static double ArctanTerm(double x, uint i) =>
-		(i % 2 == 0 ? -1 : 1) * Pow(x, 2*i - 1) / (2 * i - 1);
-
-	public static double Arctan(double x, uint n) =>
-		TaylorSeries(ArctanTerm, x, n);
-
-	private static T TaylorSeries<T>(Func<T, uint, T> term, T x, uint n)
-		where T : INumber<T>
+	public static double Sin(double x, uint n)
 	{
-		var sum = T.Zero;
+		var sum = 0.0;
+		var term = x;
+		var xSquared = x * x;
+
 		for (var i = 1u; i <= n; i++)
 		{
-			sum += term(x, i);
+			if (i != 1u)
+			{
+				term *= -xSquared;
+				term /= (2 * i - 2) * (2 * i - 1);
+			}
+
+			sum += term;
 		}
 
 		return sum;
 	}
 
-	private static uint Factorial(uint n)
+	public static double Cos(double x, uint n)
 	{
-		var value = 1u;
+		var sum = 0.0;
+		var term = 1.0;
+		var xSquared = x * x;
 
-		for (var i = 2u; i <= n; i++)
+		for (var i = 1u; i <= n; i++)
 		{
-			value *= i;
+			if (i != 1u)
+			{
+				term *= -xSquared;
+				term /= (2 * i - 3) * (2 * i - 2);
+			}
+
+			sum += term;
 		}
 
-		return value;
+		return sum;
+	}
+
+	public static double Arctan(double x, uint n)
+	{
+		var sum = 0.0;
+		var term = x;
+		var xSquared = x * x;
+
+		for (var i = 1u; i <= n; i++)
+		{
+			if (i != 1u)
+			{
+				term *= -xSquared * (2 * i - 3);
+				term /= 2 * i - 1;
+			}
+
+			sum += term;
+		}
+
+		return sum;
 	}
 
 	public static T Pow<T>(T x, uint n) where T : INumber<T>
