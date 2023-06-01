ebfb9f5cc5e2854c4b4066beb93e9da0b73d264c
Artyom Abramyan <artemabramyan7@icloud.com>
2023-06-01 17:44:05 +0300

    Написал класс `Analyzer` для анализа модели

---
+++ App/Analyzer.cs
@@ -0,0 +1,51 @@
+﻿namespace App;
+
+public static class Analyzer
+{
+	public static IEnumerable<double> AnalyzeEpsilonAndPrecisionConnection(
+		SystemParameters parameters,
+		IEnumerable<uint> precisionSamples
+	) => precisionSamples.Select(precision => FindBestFittingEpsilon(parameters, precision));
+
+	private static double FindBestFittingEpsilon(SystemParameters parameters, uint precision)
+	{
+		var upperBound = 75.0;
+		var lowerBound = 0.001;
+
+		for (var i = 0;; i++)
+		{
+			var epsilon = (upperBound + lowerBound) / 2;
+			var drawer = new IterativeLineDrawer(parameters, precision, epsilon, 1000);
+
+			Console.WriteLine(epsilon);
+
+			switch (IsReachingEndPoint(drawer), i >= 15)
+			{
+				case (true, true):
+					return epsilon;
+				case (true, false):
+					upperBound = epsilon;
+					break;
+				default:
+					lowerBound = epsilon;
+					break;
+			}
+		}
+	}
+
+	private static bool IsReachingEndPoint(IterativeLineDrawer drawer)
+	{
+		drawer.InitializeState();
+
+		while (true)
+		{
+			var status = drawer.Next();
+
+			if (status is DrawingStatus.InNeighbourhood)
+				return true;
+
+			if (status is DrawingStatus.OutOfReact)
+				return false;
+		}
+	}
+}
